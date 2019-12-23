affe2a99e3f78d8c5be2b9c5eb9a04b8e0501ed9
Nobuaki.Kajiki <nobuaki.kaiki@avanade.com>
2019-12-23 09:46:56 +0900

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour';
+  title = 'Tour of heroes';
+
+  childHogeValue:string = 'inital value';
 }
